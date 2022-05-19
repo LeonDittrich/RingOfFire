12f80e8f0fb9f49a2f58606bfcd785a39c914f3b
LeonDittrich <leon_dittrich@web.de>
2022-05-19 19:19:18 +0200

    pull shuffeld cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,6 +8,7 @@ import { Game } from 'src/models/game';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
   game: Game;  // Variable heist game vom Typ Game(game.ts)
 
   constructor() { }
@@ -24,7 +25,15 @@ export class GameComponent implements OnInit {
 
 
   takeCard() {
+    if (!this.pickCardAnimation) {  // wird nur ausgeführt wenn pickCardAnimation false ist(! ist false)
+      this.currentCard = this.game.stack.pop();  // das pop gibt uns den Letzten Wert des Array zurück und wird entfernt aus dem Array
+    console.log(this.currentCard);
     this.pickCardAnimation = true;
+
+    setTimeout(() => {
+      this.pickCardAnimation = false;
+    }, 1500);  // durch den Timeout können wir nur alle 1,5 sec eine neue Karte ziehen
+    }
   }
 
 }
